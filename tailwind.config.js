b0d35879d9d1e06a4f8609bddd32622ad81ea21a
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-06-11 21:08:39 +0530

    Add: Tailwind setup for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "button":"#5D5FEF",
+        "sidebar":"#F5F7FB",
+        "background":"#FFFFFF",
+        "sidebar-text":"#303030",
+        "school-label":"#3FB416",
+        "social-labael":"#AE924B",
+        "notification":"#B80F0F",
+        "message-notifiacation":"#D9D9D9",
+        "office-label":"#F2994A",
+      }
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 }
