6cd2ae0fa5847a42a3c3dc5e46314a1cf3116dd0
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-06-11 19:05:12 +0530

    Add: Tailwind setup for the project

--- src/views/websiteIndex.vue
+++ src/views/websiteIndex.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
+    <div>
 
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
-  setup () {
-    
+    setup () {
+        
 
-    return {}
-  }
+        return {}
+    }
 }
 </script>
 
