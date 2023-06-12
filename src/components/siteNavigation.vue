d00168d03522dfe5f2f89b621932a7a52e71a174
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-06-12 18:30:32 +0530

    Add: Final Layout of navbar

--- src/components/siteNavigation.vue
+++ src/components/siteNavigation.vue
@@ -2,30 +2,24 @@
     <header class="sticky top-0 bg-message-notifiacation shadow-lg h-20 flex items-center">
         <nav class="container flex flex-col sm: flex-row gap-4 text-black py-6">
             <RouterLink to="{ name: 'home'}">
-                <div class="flex items-center gap-3 flex-1">
+                <div class="flex items-center gap-3">
                     <img align-lest width="64" height="64" src="https://img.icons8.com/nolan/64/gmail-new.png"
                         alt="gmail-new" />
                     <p class="text-2xl">SMAIL</p>
+                    <div class="flex gap-3 flex-1 justify-end">
+                        <button
+                            class="mx-2 my-2 bg-gray-200 transition duration-150 ease-in-out focus:outline-none hover:bg-button hover:text-white rounded text-indigo-700 px-6 py-2 text-xs">Sign In</button>
+                    </div>
+                    <button class="mx-2 my-2 bg-indigo-700 transition duration-150 ease-in-out hover:bg-white hover:text-button rounded text-white px-6 py-2 text-xs">Create an account</button>
                 </div>
             </RouterLink>
-            <div class="nav_aside">
-                <div class="nav_aside_buttons">
-                    <div class="q-pa-md q-gutter-sm">
-                        <q-btn color="purple" label="Purple" />
-                    </div>
-                </div>
-            </div>
         </nav>
     </header>
 </template>
 
 <script>
-import QBtn from 'quasar'
 import { RouterLink } from 'vue-router'
 export default {
-    components: {
-    QBtn
-  },
     setup() {
 
 
