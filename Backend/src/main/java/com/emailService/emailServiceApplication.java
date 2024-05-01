f661766a4aa10cfcf7b385b485ee0e06d36bc84c
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-05-01 16:28:38 +0530

    Add : Created model, two entities named user, email

---
+++ Backend/src/main/java/com/emailService/emailServiceApplication.java
@@ -0,0 +1,11 @@
+package com.emailService;
+
+import org.springframework.boot.SpringApplication;
+
+public class emailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(emailServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/src/main/java/com/emailService/model/email.java
@@ -0,0 +1,76 @@
+package com.emailService.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "emails")
+public class email {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "sender_id", referencedColumnName = "id")
+    private user sender;
+    @ManyToOne
+    @JoinColumn(name = "sender_id", referencedColumnName = "id")
+    private user receiver;
+    @Column(nullable = false)
+    private String subject;
+    @Column(nullable = false)
+    private String body;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date sentDate;
+
+    public email(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public user getSender() {
+        return sender;
+    }
+
+    public void setSender(user sender) {
+        this.sender = sender;
+    }
+
+    public user getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiverr(user receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getSentDate() {
+        return sentDate;
+    }
+
+    public void setSentDate(Date sentDate) {
+        this.sentDate = sentDate;
+    }
+}
