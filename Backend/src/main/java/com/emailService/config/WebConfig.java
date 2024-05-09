a0170f4f70d9ee39d4cd70f96d9ba722f0d7324f
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-05-09 12:39:16 +0530

    Fix : Deleted unnecessary files

---
+++ Backend/src/main/java/com/emailService/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.emailService.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")  // URL of the Vue.js frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
